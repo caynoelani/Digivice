ad370135add891b0016ec70726f3a3b31cd5fdd7
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-09-27 17:25:24 -0500

    file structure

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask_app import app
+from flask import render_template
 
 @app.route('/')
 def server_test():
-    return "Hello World"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

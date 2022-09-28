540b82747c0eb14e4cb7c816907d1be7945888ca
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-09-28 15:36:03 -0500

    update server; modularize controllers

--- flask_app/controllers/main_controller.py
+++ flask_app/controllers/main_controller.py
@@ -1,3 +1,19 @@
 from flask_app import app
 from flask import redirect, render_template, session
 from flask_app.models import user_model, digimon_model
+
+@app.route('/')
+def index():
+    return render_template('index.html', view = 'index')
+
+@app.route('/favorites')
+def favorites():
+    return render_template('favorites.html', view = 'favorites')
+
+@app.route('/catalogue')
+def catalogue():
+    return render_template('catalogue.html', view = 'catalogue')
+
+@app.route('/digimon')
+def digimon():
+    return render_template('digimon.html', view = 'digimon')
--- server.py
+++ server.py
@@ -1,21 +1,6 @@
 from flask_app import app
-from flask import render_template
 
-@app.route('/')
-def index():
-    return render_template('index.html', view = 'index')
-
-@app.route('/favorites')
-def favorites():
-    return render_template('favorites.html', view = 'favorites')
-
-@app.route('/catalogue')
-def catalogue():
-    return render_template('catalogue.html', view = 'catalogue')
-
-@app.route('/digimon')
-def digimon():
-    return render_template('digimon.html', view = 'digimon')
+from flask_app.controllers import main_controller, user_controller, digimon_controller
 
 if __name__ == '__main__':
     app.run(debug=True)

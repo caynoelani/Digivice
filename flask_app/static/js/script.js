d570d7167088da239ea7a08cc05e1f0c079ce8a0
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-09-27 21:09:34 -0500

    header

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -2,5 +2,9 @@ const navMobile = document.getElementById("navMobile");
 const navList = document.getElementById("navList");
 
 navMobile.addEventListener("click", () => {
-    navList.classList.toggle("active")
+    navList.classList.toggle("active");
 })
+
+document.querySelectorAll(".nav__list-link").forEach( e => e.addEventListener("click", () => {
+    navList.classList.remove("active");
+}))

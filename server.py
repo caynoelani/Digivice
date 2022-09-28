cd952c2bd9e7af1454224bc8d198c621585ba97d
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-09-28 09:22:41 -0500

    Dynamic Header

--- server.py
+++ server.py
@@ -2,8 +2,20 @@ from flask_app import app
 from flask import render_template
 
 @app.route('/')
-def server_test():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', view = 'index')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', view = 'about')
+
+@app.route('/catalogue')
+def catalogue():
+    return render_template('catalogue.html', view = 'catalogue')
+
+# @app.route('/catalogue/<id>')
+# def catalogue(id):
+#     return render_template('index.html', view = 'catalogue/<id>')
 
 if __name__ == '__main__':
     app.run(debug=True)

b8008bf0ea5adb8853fc6a1411531413c4c97d8b
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-09-30 18:44:03 -0500

    Implement Environment Variables

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
-app.secret_key = '3948ubrf34o87yg'
+app.secret_key = os.getenv("SECRET_KEY")
--- server.py
+++ server.py
@@ -2,5 +2,8 @@ from flask_app import app
 
 from flask_app.controllers import main_controller, user_controller, digimon_controller
 
+from dotenv import load_dotenv
+load_dotenv()
+
 if __name__ == '__main__':
     app.run(debug=True)

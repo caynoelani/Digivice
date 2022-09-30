f2881296d79eb5f334cec67398172c7d7076e724
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-09-30 11:48:33 -0500

    navbar login functionality

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -1,6 +1,9 @@
 const navMobile = document.getElementById("navMobile");
 const navList = document.getElementById("navList");
 
+
+// NAVBAR FUNCTIONALITY
+
 navMobile.addEventListener("click", () => {
     navList.classList.toggle("active");
 })
@@ -8,3 +11,4 @@ navMobile.addEventListener("click", () => {
 document.querySelectorAll(".nav__list-link").forEach( e => e.addEventListener("click", () => {
     navList.classList.remove("active");
 }))
+

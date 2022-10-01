6a16cc6fdb9a764c49dbd25d5af4c6f87b79abb7
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-10-01 13:37:42 -0500

    Login/Reg Form Validations

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -28,16 +28,42 @@ from flask_app.models import user_model
 #=====================================
 @app.route('/login')
 def render_login():
+
+    if "user_id" in session:
+        return redirect('/')
+
     return render_template('login.html')
 
 @app.route('/login', methods=["POST"])
 def user_login():
     
     if request.form["form_type"] == "login":
-        session["email"] = request.form["email"]
+        if not user_model.User.validate_login(request.form):
+            return redirect ('/')
+
+        data = { "email" : request.form["email"] }
+        user = user_model.User.get_by_email(data)
+
+        session["user_id"] = user.id
+        session["username"] = user.username
+
     elif request.form["form_type"] == "register":
+        if not user_model.User.validate_register(request.form):
+            return redirect ('/')
+
+        pw_hash = bcrypt.generate_password_hash(request.form['password'])
+
+        data = {
+            "username" : request.form['username'],
+            "email" : request.form['email'],
+            "password" : pw_hash
+        }
+
+        user_id = user_model.User.create(data)
+
+        session['user_id'] = user_id
         session["username"] = request.form["username"]
-        session["email"] = request.form["email"]
+
     return redirect('/')
 
 #=====================================
@@ -45,6 +71,6 @@ def user_login():
 #=====================================
 @app.route('/logout', methods=['POST'])
 def logout():
+    
     session.clear()
-    print("user logged out")
     return redirect('/')
--- flask_app/models/user_model.py
+++ flask_app/models/user_model.py
@@ -49,6 +49,69 @@ class User:
 #***************STATIC METHODS****************
 #*********************************************
 
+#===================================
+# Validate Register
+#===================================
+   @staticmethod
+   def validate_register(data):
+      is_valid = True
+
+      if len(data['first_name']) < 2:
+         flash("First name must be at least 2 characters.")
+         is_valid = False
+      elif not NAME_REGEX.match(data['first_name']): 
+         flash("First name must be letters only!")
+         is_valid = False
+
+
+      if len(data['last_name']) < 2:
+         flash("Last name must be at least 2 characters.")
+         is_valid = False
+      elif not NAME_REGEX.match(data['last_name']): 
+         flash("Last name must be letters only!")
+         is_valid = False
+
+
+      if not EMAIL_REGEX.match(data['email']): 
+         flash("Invalid email address!")
+         is_valid = False
+      elif EMAIL_REGEX.match(data['email']): 
+         query = "SELECT * FROM users;"
+         results = connectToMySQL(User.db).query_db(query)
+
+         for dictionary in results:
+               if data['email'] == dictionary['email']:
+                  flash("email already exists")
+                  is_valid = False
+
+
+      if data['password'] != data['password_confirmation']:
+         flash("Passwords do not match.")
+         is_valid = False
+      if not PASSWORD_REGEX.match(data['password']): 
+         flash("Password must have at least one uppercase letter, one lowercase letter, one number, and be 8-255 characters in length!")
+         is_valid = False
+
+      return is_valid
+
+#===================================
+# Validate Login
+#===================================
+   @staticmethod
+   def validate_login(data):
+      is_valid = True
+
+      user_in_db = User.get_by_email(data)
+
+      if not user_in_db:
+         flash("Invalid email or password")
+         is_valid = False
+      
+      elif not bcrypt.check_password_hash(user_in_db.password, data['password']):
+         flash("Invalid email or password")
+         is_valid = False
+         
+      return is_valid
 
 
 #*********************************************

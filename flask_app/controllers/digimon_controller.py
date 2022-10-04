0a2d0256d373647fb7100a1e2fd0a328593e0b61
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-10-04 13:36:15 -0500

    Dynamic Attributes

--- flask_app/controllers/digimon_controller.py
+++ flask_app/controllers/digimon_controller.py
@@ -18,7 +18,6 @@ import requests, json, os
 #=====================================
 from flask_app.models import favorite_model, user_model, digimon_model
 
-
 #******************************************************
 #***********************ROUTES*************************
 #******************************************************
@@ -31,7 +30,10 @@ async def search_digimon():
 
     req = request.form["indexSearchbar"]
 
-    if req.isalpha():
+    if req.isnumeric():
+        return redirect(f'/digimon/{int(req)}')
+
+    elif req.isalpha():
         try:
             req = req.lower()
             return redirect(f'/digimon/{req}')
@@ -39,9 +41,6 @@ async def search_digimon():
         except requests.exceptions.JSONDecodeError:
             flash('Please check your spelling and try again')
             return redirect('/')
-
-    if req.isnumeric():
-        return redirect(f'/digimon/{int(req)}')
 
     else:
         flash("Please enter a digimon's name or number")
@@ -64,25 +63,17 @@ async def search_digimon():
 @app.route('/digimon/<int:req>',)
 async def read_one_digimon_int(req):
 
-    print(f"req is {req}")
-
     is_logged_in = user_model.User.validate_logged_in()
 
     digimon_info = await digimon_model.get_digimon_info(req)
-
-    print(digimon_info)
 
     return render_template('digimon.html', is_logged_in = is_logged_in, digimon_info = digimon_info)
 
 @app.route('/digimon/<req>',)
 async def read_one_digimon_name(req):
 
-    print(f"req is {req}")
-
     is_logged_in = user_model.User.validate_logged_in()
 
     digimon_info = await digimon_model.get_digimon_info(req)
-
-    print(digimon_info)
 
     return render_template('digimon.html', is_logged_in = is_logged_in, digimon_info = digimon_info)

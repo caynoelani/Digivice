944cb9ae5cbc3f706595f8c042620fa9c40807ea
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-10-06 11:40:07 -0500

    Add favorite method with validation

--- flask_app/models/favorite_model.py
+++ flask_app/models/favorite_model.py
@@ -27,7 +27,7 @@ class Favorite:
 
         self.user_id = data["user_id"] #foreign key
 
-        self.number = data["digimon_id"]
+        self.number = data["number"]
 
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
@@ -47,29 +47,51 @@ class Favorite:
     #=============================
     @classmethod
     def create_favorite(cls, data):
+        favorite = cls.get_favorite_by_number(data)
+
+        if favorite:
+            print("already favorited")
+            return False
+            
         query = "INSERT INTO favorites (number, created_at, updated_at, user_id) VALUES (%(digimon_id)s, NOW(), NOW(), %(user_id)s)"
 
         results = connectToMySQL(cls.db).query_db(query, data)
 
         return results
 
+    # #=============================
+    # # READ (GET ALL) Favorites
+    # #=============================
+    # @classmethod
+    # def get_favorite_by_user(cls, data):
+
+    #     query = "SELECT * FROM favorites WHERE user_id = %(id)s"
+    #     results = connectToMySQL(cls.db).query_db(query, data)
+
+    #     favorites_list = []
+
+    #     if not results:
+    #         return False
+        
+    #     for digimon in results:
+    #         favorites_list.append(cls(digimon))
+        
+    #     return favorites_list
+
     #=============================
-    # READ (GET ALL) Favorites
+    # READ (one) by id
     #=============================
     @classmethod
-    def get_favorite_by_user(cls, data):
+    def get_favorite_by_number(cls, data):
 
-        query = "SELECT * FROM favorites WHERE user_id = %(id)s"
+        query = "SELECT * FROM favorites WHERE number = %(digimon_id)s;"
+
         results = connectToMySQL(cls.db).query_db(query, data)
+        print(results)
 
-        favorites_list = []
-
-        if not results:
+        if len(results) < 1:
             return False
-        
-        for digimon in results:
-            favorites_list.append(cls(digimon))
-        
-        return favorites_list
+            
+        return cls(results[0])
 
 #    SELECT * FROM users JOIN favorites ON users.id = favorites.user_id WHERE users.id = %(id)s

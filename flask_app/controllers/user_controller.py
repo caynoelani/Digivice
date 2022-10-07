95fea3e063d910865aed024fc4683885bb86409e
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-10-07 15:12:30 -0500

    Consolidate login post and get routes

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -24,49 +24,49 @@ from flask_app.models import user_model
 #******************************************************
 
 #=====================================
-# Login Routes
+# Login/Register Route
 #=====================================
-@app.route('/login')
-def render_login():
 
-    is_logged_in = user_model.User.validate_logged_in()
-
-    if is_logged_in:
-        return redirect('/favorites')
-
-    return render_template('login.html', is_logged_in = is_logged_in)
-
-@app.route('/login', methods=["POST"])
+@app.route('/login', methods=["POST", 'GET'])
 def user_login():
     
-    if request.form["form_type"] == "login":
-        if not user_model.User.validate_login(request.form):
-            return redirect ('/login')
+    if request.method == 'POST':
+        if request.form["form_type"] == "login":
+            if not user_model.User.validate_login(request.form):
+                return redirect ('/login')
 
-        data = { "email" : request.form["email"] }
-        user = user_model.User.get_user_by_email(data)
+            data = { "email" : request.form["email"] }
+            user = user_model.User.get_user_by_email(data)
 
-        session["user_id"] = user.id
-        session["username"] = user.username
+            session["user_id"] = user.id
+            session["username"] = user.username
 
-    elif request.form["form_type"] == "register":
-        if not user_model.User.validate_register(request.form):
-            return redirect ('/login')
+        elif request.form["form_type"] == "register":
+            if not user_model.User.validate_register(request.form):
+                return redirect ('/login')
 
-        pw_hash = bcrypt.generate_password_hash(request.form['password'])
+            pw_hash = bcrypt.generate_password_hash(request.form['password'])
 
-        data = {
-            "username" : request.form['username'],
-            "email" : request.form['email'],
-            "password" : pw_hash
-        }
+            data = {
+                "username" : request.form['username'],
+                "email" : request.form['email'],
+                "password" : pw_hash
+            }
 
-        user_id = user_model.User.create_user(data)
+            user_id = user_model.User.create_user(data)
 
-        session['user_id'] = user_id
-        session["username"] = request.form["username"]
+            session['user_id'] = user_id
+            session["username"] = request.form["username"]
 
-    return redirect('/favorites')
+        return redirect('/favorites')
+    
+    else:
+        is_logged_in = user_model.User.validate_logged_in()
+
+        if is_logged_in:
+            return redirect('/favorites')
+
+        return render_template('login.html', is_logged_in = is_logged_in)
 
 #=====================================
 # Logout Route

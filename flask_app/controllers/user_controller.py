4d5ad2406a08257ad1716960f0f48df2d451773e
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-10-14 18:25:44 -0500

    debug empty favorites list

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -40,49 +40,51 @@ def login_required(func):
     return wrapper
 
 #=====================================
-# Login/Register Route
+# Login/Register Route (GET)
 #=====================================
-
-@app.route('/login', methods=["POST", 'GET'])
+@app.route('/login')
 def login_page():
-    
-    if request.method == 'POST':
-        if request.form["form_type"] == "login":
-            if not user_model.User.validate_login(request.form):
-                return redirect ('/login')
-
-            data = { "email" : request.form["email"] }
-            user = user_model.User.get_user_by_email(data)
-
-            session["user_id"] = user.id
-            session["username"] = user.username
-
-        elif request.form["form_type"] == "register":
-            if not user_model.User.validate_register(request.form):
-                return redirect ('/login')
-
-            pw_hash = bcrypt.generate_password_hash(request.form['password'])
-
-            data = {
-                "username" : request.form['username'],
-                "email" : request.form['email'],
-                "password" : pw_hash
-            }
-
-            user_id = user_model.User.create_user(data)
-
-            session['user_id'] = user_id
-            session["username"] = request.form["username"]
-
-        return redirect('/favorites')
-    
-    else:
         is_logged_in = user_model.User.validate_logged_in()
 
         if is_logged_in:
             return redirect('/favorites')
 
         return render_template('login.html', is_logged_in = is_logged_in)
+
+#=====================================
+# Login/Register Route (POST)
+#=====================================
+@app.route('/login', methods=["POST"])
+def login_post():
+
+    if request.form["form_type"] == "login":
+        if not user_model.User.validate_login(request.form):
+            return redirect ('/login')
+
+        data = { "email" : request.form["email"] }
+        user = user_model.User.get_user_by_email(data)
+
+        session["user_id"] = user.id
+        session["username"] = user.username
+
+    elif request.form["form_type"] == "register":
+        if not user_model.User.validate_register(request.form):
+            return redirect ('/login')
+
+        pw_hash = bcrypt.generate_password_hash(request.form['password'])
+
+        data = {
+            "username" : request.form['username'],
+            "email" : request.form['email'],
+            "password" : pw_hash
+        }
+
+        user_id = user_model.User.create_user(data)
+
+        session['user_id'] = user_id
+        session["username"] = request.form["username"]
+
+    return redirect('/favorites')
 
 #=====================================
 # Logout Route
@@ -92,4 +94,4 @@ def login_page():
 def logout():
     
     session.clear()
-    return redirect('/')
+    return redirect(url_for('login_page'))

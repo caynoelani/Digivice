d7ab698ec85494c4b624938b507cfb5f4051e7cd
Cayla Bradley <cayla.bradley94@gmail.com>
2022-10-20 12:27:28 -0500

    build login/reg toggle functionality
    Enable users to toggle between login and register forms using the toggle button at the top of the page

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -2,8 +2,14 @@
 const navMobile = document.getElementById("navMobile");
 const navList = document.getElementById("navList");
 
+//LOGIN/REG VARIABLES
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
+const loginBtn = document.getElementById("loginBtn");
+const registerBtn = document.getElementById("registerBtn");
 
-// NAVBAR FUNCTIONALITY
+
+//NAVBAR FUNCTIONALITY
 navMobile.addEventListener("click", () => {
     navList.classList.toggle("active");
 })
@@ -12,3 +18,18 @@ document.querySelectorAll(".nav__list-link").forEach( e => e.addEventListener("c
     navList.classList.remove("active");
 }))
 
+
+//LOGIN/REG FUNCTIONALITY
+loginBtn.addEventListener("click", () => {
+    loginBtn.classList.toggle("active");
+    registerBtn.classList.toggle("active");
+    loginForm.classList.toggle("active");
+    registerForm.classList.toggle("active");
+})
+
+registerBtn.addEventListener("click", () => {
+    loginBtn.classList.toggle("active");
+    registerBtn.classList.toggle("active");
+    loginForm.classList.toggle("active");
+    registerForm.classList.toggle("active");
+})
